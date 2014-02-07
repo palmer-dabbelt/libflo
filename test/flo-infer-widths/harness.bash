08991914de783971252e787e552dfd41eaf8f5ec
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-02-07 14:21:57 -0800

    Have test output be more verbose
    This actually shows what's run and output from the tests, to aid
    debugging.

--- test/flo-infer-widths/harness.bash
+++ test/flo-infer-widths/harness.bash
@@ -3,6 +3,9 @@ $PTEST_BINARY test.in > test.out
 cat test.out | sort > test.out.sort
 cat test.gold | sort > test.gold.sort
 
+cat test.out
+cat test.gold
+
 diff -au test.gold.sort test.out.sort
 exit $?
 
