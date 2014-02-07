609e46c40abffdf48a2a2d55bb0a8ddedc7cb14c
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-02-07 14:45:23 -0800

    Add two more simple test cases

---
+++ test/flo-infer-widths/counter-16.bash
@@ -0,0 +1,19 @@
+#include "tempdir.bash"
+
+cat >test.in <<EOF
+reset = rst
+T0 = add/16 Counter16::reg 1
+Counter16::reg__update = mux reset 0 T0
+Counter16::reg = reg T0 Counter16::reg__update
+Counter16::io_count = out/16 Counter16::reg
+EOF
+
+cat >test.gold <<EOF
+reset = rst/1
+T0 = add/16 Counter16::reg 1
+Counter16::reg__update = mux/16 reset 0 T0
+Counter16::reg = reg/16 T0 Counter16::reg__update
+Counter16::io_count = out/16 Counter16::reg
+EOF
+
+#include "harness.bash"
---
+++ test/flo-infer-widths/counter-5.bash
@@ -0,0 +1,19 @@
+#include "tempdir.bash"
+
+cat >test.in <<EOF
+reset = rst
+T0 = add/5 Counter5::reg 1
+Counter5::reg__update = mux reset 0 T0
+Counter5::reg = reg T0 Counter5::reg__update
+Counter5::io_count = out/5 Counter5::reg
+EOF
+
+cat >test.gold <<EOF
+reset = rst/1
+T0 = add/5 Counter5::reg 1
+Counter5::reg__update = mux/5 reset 0 T0
+Counter5::reg = reg/5 T0 Counter5::reg__update
+Counter5::io_count = out/5 Counter5::reg
+EOF
+
+#include "harness.bash"
